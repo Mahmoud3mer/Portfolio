d638670d5862fed7fffbf3c8ed390b2b146c4c58
Mahmoud Amer <m.amer0440@gmail.com>
2025-06-30 02:59:38 +0300

    Update Font

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,9 +4,11 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairsTransition from "@/components/StairsTransition";
 
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] });
 
 const JetbrainsMono = JetBrains_Mono({
-  variable: "--font-JetBrainsMono",
   subsets: ["latin"],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
 });
@@ -20,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${JetbrainsMono.variable} px-4`}
+        className={`${JetbrainsMono.className} px-4`}
       >
         <Header />
 
