6ff1277eff44045086ec7ee96b507ff5e77534a1
Mahmoud Amer <m.amer0440@gmail.com>
2025-07-06 07:35:30 +0300

    Update work page.

--- app/(pages)/work/page.jsx
+++ app/(pages)/work/page.jsx
@@ -10,7 +10,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 // import Swiper styles
 import 'swiper/css';
 
@@ -24,7 +24,7 @@ const projects = [
     category: 'Fullstack',
     title: 'Online Library',
     description: 'An online platform for browsing, reviewing, and purchasing books with fullstack functionalities.',
-    stack: [{name: 'Html 5'}, {name: 'CSS'},{name: 'Javascript'}, {name: 'TailwindCSS'}, {name: 'Angular.js'}, {name: 'React.js'}, {name: 'Node.js'},{name: 'Nest.js'}, {name: 'Mongodb'}, ],
+    stack: [{ name: 'Html 5' }, { name: 'CSS' }, { name: 'Javascript' }, { name: 'TailwindCSS' }, { name: 'Angular.js' }, { name: 'React.js' }, { name: 'Node.js' }, { name: 'Nest.js' }, { name: 'Mongodb' },],
     image: '/work/online-library2.png',
     live: 'https://andalosia.vercel.app/home',
     github: 'https://github.com/Mahmoud3mer/Online-Library',
@@ -32,29 +32,38 @@ const projects = [
   {
     num: '02',
     category: 'Frontend',
-    title: 'Amer E-Commerce',
-    description: 'A responsive e-commerce web application with product management and shopping cart features.',
-    stack: [{name: 'Html 5'}, {name: 'CSS'},{name: 'Typescript'}, {name: 'React-Bootstrap'}, {name: 'React.js'}, {name: 'React-hook-form'}, {name: 'Redux'}, {name: 'Redux-persist'}, {name: 'JSON-Server'}, ],
-    image: '/work/e-commerce-amer2.png',
-    live: 'https://amer-e-com-web-app.vercel.app/',
-    github: 'https://github.com/Mahmoud3mer/Amer-ECom-Web-App',
+    title: 'Rawafed Demo',
+    description: 'It is a smart system that connects domestic workers directly with government, private, and foreign institutions and clients, ensuring accurate and smooth monitoring of all stages of recruitment.',
+    stack: [{ name: 'Html 5' }, { name: 'CSS' }, { name: 'JQuery' }, { name: 'Laravel' }],
+    image: '/work/rawafed.png',
+    live: 'https://rawfed.sa/ar',
   },
   {
     num: '03',
     category: 'Frontend',
-    title: 'Portfolio',
-    description: 'A personal portfolio website showcasing projects, skills, and contact information.',
-    stack: [{name: 'Html 5'}, {name: 'CSS'},{name: 'Javascript'}, {name: 'TailwindCSS'}, {name: 'Next.js'}, {name: 'React-hook-form'}, ],
-    image: '/work/portfolio2.png',
+    title: 'Amer E-Commerce',
+    description: 'A responsive e-commerce web application with product management and shopping cart features.',
+    stack: [{ name: 'Html 5' }, { name: 'CSS' }, { name: 'Typescript' }, { name: 'React-Bootstrap' }, { name: 'React.js' }, { name: 'React-hook-form' }, { name: 'Redux' }, { name: 'Redux-persist' }, { name: 'JSON-Server' },],
+    image: '/work/e-commerce-amer2.png',
     live: 'https://amer-e-com-web-app.vercel.app/',
     github: 'https://github.com/Mahmoud3mer/Amer-ECom-Web-App',
   },
   {
     num: '04',
     category: 'Frontend',
+    title: 'Portfolio',
+    description: 'A personal portfolio website showcasing projects, skills, and contact information.',
+    stack: [{ name: 'Html 5' }, { name: 'CSS' }, { name: 'Javascript' }, { name: 'TailwindCSS' }, { name: 'Next.js' }, { name: 'React-hook-form' },],
+    image: '/work/portfolio2.png',
+    live: 'https://amer-e-com-web-app.vercel.app/',
+    github: 'https://github.com/Mahmoud3mer/Amer-ECom-Web-App',
+  },
+  {
+    num: '05',
+    category: 'Frontend',
     title: 'To Do List',
     description: 'A simple and clean to-do list app for managing daily tasks with a modern UI.',
-    stack: [{name: 'Html 5'}, {name: 'CSS'},{name: 'Typescript'}, {name: 'MUI'}, {name: 'React.js'}, ],
+    stack: [{ name: 'Html 5' }, { name: 'CSS' }, { name: 'Typescript' }, { name: 'MUI' }, { name: 'React.js' },],
     image: '/work/todo-list.png',
     live: 'https://todo-list-amer.netlify.app/',
     github: 'https://github.com/Mahmoud3mer/TODO-LIST',
@@ -63,7 +72,7 @@ const projects = [
 
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
-  
+
   const handleSlideChange = (e) => {
     const activeIndex = e.activeIndex;
 
@@ -73,8 +82,8 @@ const Work = () => {
   return (
     <section>
       <motion.div
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{
           duration: 0.4,
           delay: 2,
@@ -100,7 +109,7 @@ const Work = () => {
               </p>
               <ul className='flex flex-wrap gap-4'>
                 {
-                  project?.stack?.map((st, i) => 
+                  project?.stack?.map((st, i) =>
                     <li key={i} className='text-accentDefault hover:text-accentHover'>
                       {
                         st?.name
@@ -116,7 +125,7 @@ const Work = () => {
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger className='h-[70px] w-[70px] rounded-full bg-white/5 flex justify-center items-center cursor-pointer'>
-                        <BsArrowUpRight className='text-3xl hover:text-accentHover duration-400 transition-all hover:rotate-45'/>
+                        <BsArrowUpRight className='text-3xl hover:text-accentHover duration-400 transition-all hover:rotate-45' />
                       </TooltipTrigger>
 
                       <TooltipContent>
@@ -126,19 +135,22 @@ const Work = () => {
                   </TooltipProvider>
                 </Link>
 
-                <Link href={project.github} target='blank'>
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger className='h-[70px] w-[70px] rounded-full bg-white/5 flex justify-center items-center cursor-pointer'>
-                        <BsGithub className='text-3xl hover:text-accentHover duration-400 transition-all'/>
-                      </TooltipTrigger>
+                {
+                  project.github ?
+                    <Link href={project.github} target='blank'>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger className='h-[70px] w-[70px] rounded-full bg-white/5 flex justify-center items-center cursor-pointer'>
+                            <BsGithub className='text-3xl hover:text-accentHover duration-400 transition-all' />
+                          </TooltipTrigger>
 
-                      <TooltipContent>
-                        <p>Github Repository</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
+                          <TooltipContent>
+                            <p>Github Repository</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </Link> : null
+                }
               </div>
             </div>
 
@@ -151,14 +163,14 @@ const Work = () => {
                 className='xl:h-[520px] mb-12 relative'
               >
                 {
-                  projects.map((p, i) => 
+                  projects.map((p, i) =>
                     <SwiperSlide key={i} className='w-full'>
                       <div className='bg-pink-50/20 w-full h-[460px] relative group'>
                         {/* overlay */}
                         <div className='bg-black/10 absolute top-0 bottom-0 z-10 w-full h-full'></div>
                         {/* Image */}
                         <div className='w-full h-full'>
-                          <Image 
+                          <Image
                             src={project.image}
                             alt={project.title}
                             fill
@@ -166,7 +178,7 @@ const Work = () => {
                             className='object-cover'
                           />
                         </div>
-                       
+
                       </div>
                     </SwiperSlide>
                   )
